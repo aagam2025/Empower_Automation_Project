54976a522c6b3eb128b2c365ea19aec6c3873a65
aagam2025 <aagam.d@empowerelearning.com>
2026-02-13 14:12:11 +0530

    Update library_page.py

--- pages/library_page.py
+++ pages/library_page.py
@@ -14,9 +14,9 @@ class library(BasePage):
     def read_library(self):
         self.page.wait_for_selector(self.read_library_btn)
         self.page.click(self.read_library_btn)
-        self.page.wait_for_load_state("networkidle")  # Better than "load" for PDFs
-        self.page.wait_for_timeout(2000)  # Brief pause for PDF render
+        self.page.wait_for_load_state("networkidle")
+        self.page.wait_for_timeout(2000)
 
-        for _ in range(15):  # 15 small scrolls ~3 pages
-            self.page.mouse.wheel(0, 200)  # 200px per scroll
-            self.page.wait_for_timeout(1500)  # Natural scroll speed   
+        for _ in range(15):
+            self.page.mouse.wheel(0, 200)
+            self.page.wait_for_timeout(1500)
